deb63521cacb86b8bd8c57f8c4d11493d3411819
Guillermo Bautista <100383477@alumnos.uc3m.es>
2020-03-04 11:48:33 +0100

    comparadores de librerias

--- data.py
+++ data.py
@@ -3,6 +3,20 @@ class Libreria:
         self.libros = libros
         self.sp = sp
         self.p = p
+        self.importancia = 0
+        
+    #para poder ordenar llista de librerias por importancia
+    def __lt__(self, other):
+        return self.importancia<other.importancia        
+    
+    def __gt__(self, other):
+        return self.importancia>other.importancia
+
+    def __eq__(self, other):
+        return self.importancia==other.importancia and self.p == other.p and self.libros==other.libros and self.sp==other.sp
+
+    def __ne__(self, other):
+        return self.importancia!=other.importancia and self.p != other.p and self.libros!=other.libros and self.sp!=other.sp
 
 class Libro:
     def __init__(self,id, score):
